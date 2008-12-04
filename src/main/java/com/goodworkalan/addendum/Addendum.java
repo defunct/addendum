950e1d08c39e9de7c4846360d4c7f8054587d92b
Alan Gutierrez <alan@blogometer.com>
2008-12-04 01:28:56 -0600

    - Fixed description of project in pom.xml. - Added some Javadoc to Addendum interface.

--- src/main/java/com/goodworkalan/addendum/Addendum.java
+++ src/main/java/com/goodworkalan/addendum/Addendum.java
@@ -1,14 +1,19 @@
 /* Copyright Alan Gutierrez 2006 */
-/**
- * 
- */
 package com.goodworkalan.addendum;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/**
+ * Performs a single update an aspect of a web applicaiton's configuration,
+ * database or data files.
+ * 
+ * @author Alan Gutierrez
+ */
 public interface Addendum
 {
-    public void execute(Connection connnection) throws SQLException, AddendumException;
+    public void execute(Connection connnection)
+        throws SQLException, AddendumException;
 }
+
 /* vim: set et sw=4 ts=4 ai tw=78 nowrap: */
