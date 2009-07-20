376686ef79e82373f0df30686e9f042b486c41a6
Alan Gutierrez <alan@blogometer.com>
2009-07-19 20:27:03 -0500

    - Javadoc for RenameTable.

--- src/main/java/com/goodworkalan/addendum/RenameTable.java
+++ src/main/java/com/goodworkalan/addendum/RenameTable.java
@@ -3,20 +3,47 @@ package com.goodworkalan.addendum;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/**
+ * Performs a single table rename update against the database.
+ *
+ * @author Alan Gutierrez
+ */
 class RenameTable implements Update
 {
-    private final String oldName;
+    /** The existing table name. */
+    private final String name;
     
+    /** The new table name. */
     private final String newName;
-    
-    public RenameTable(String oldName, String newName)
+
+    /**
+     * The existing table name.
+     * 
+     * @param name
+     *            The existing table name.
+     * @param newName
+     *            The old table name.
+     */
+    public RenameTable(String name, String newName)
     {
-        this.oldName = oldName;
+        this.name = name;
         this.newName = newName;
     }
 
+    /**
+     * Perform a single table rename update against the database.
+     * 
+     * @param connection
+     *            The JDBC connection.
+     * @param dialect
+     *            The SQL dialect.
+     * @throws SQLException
+     *             For any SQL error.
+     * @throws AddendumException
+     *             For any error occurring during the update.
+     */
     public void execute(Connection connection, Dialect dialect) throws SQLException
     {
-        dialect.renameTable(connection, oldName, newName);
+        dialect.renameTable(connection, name, newName);
     }
 }
