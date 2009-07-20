b7a88d43ec921e52988f85515bb71cce56ea2236
Alan Gutierrez <alan@blogometer.com>
2009-07-19 19:27:10 -0500

    - Javadoc for Extend.

--- src/main/java/com/goodworkalan/addendum/Execute.java
+++ src/main/java/com/goodworkalan/addendum/Execute.java
@@ -1,9 +1,9 @@
 package com.goodworkalan.addendum;
 
 /**
- * The initial interface exposed by the {@link Addendum} class, that extends
- * other winnowing interfaces that guide the domain-specific language through
- * the required order for migration action types.
+ * An interface exposed by the {@link Addendum} class, that extends other
+ * winnowing interfaces that guide the domain-specific language through the
+ * required order for migration action types.
  * 
  * @author Alan Gutierrez
  */
--- src/main/java/com/goodworkalan/addendum/Extend.java
+++ src/main/java/com/goodworkalan/addendum/Extend.java
@@ -1,6 +1,22 @@
 package com.goodworkalan.addendum;
 
+/**
+ * The initial interface exposed by the {@link Addendum} class, that extends
+ * other winnowing interfaces that guide the domain-specific language through
+ * the required order for migration action types.
+ * 
+ * @author Alan Gutierrez
+ */
 public interface Extend extends Execute
 {
+    /**
+     * Begin an extension element.
+     * 
+     * @param <T>
+     *            The extension element type.
+     * @param extension
+     *            An instance of the extension element.
+     * @return The extension element.
+     */
     public <T extends ExtensionElement> T run(T extension);
 }
