c45d39a42b08ad2e65e6cfd04e8bb7b9be741fed
Alan Gutierrez <alan@blogometer.com>
2009-07-19 20:35:39 -0500

    - Javadoc for TableAlteration.

--- src/main/java/com/goodworkalan/addendum/TableAlteration.java
+++ src/main/java/com/goodworkalan/addendum/TableAlteration.java
@@ -4,18 +4,45 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
+/**
+ * Perform a table alteration update against the database.
+ * 
+ * @author Alan Gutierrez
+ */
 class TableAlteration implements Update
 {
+    /** The table name. */
     private final String tableName;
 
+    /** List of columns to create. */
     private final List<Column> addColumns;
 
-    public TableAlteration(String tableName, List<Column> updateColumns)
+    /**
+     * Create a table alteration that adds the given columns the given table.
+     * 
+     * @param tableName
+     *            The table name.
+     * @param addColumns
+     *            The columns to add.
+     */
+    public TableAlteration(String tableName, List<Column> addColumns)
     {   
         this.tableName = tableName;
-        this.addColumns = updateColumns;
+        this.addColumns = addColumns;
     }
-    
+
+    /**
+     * Perform a table alteration that adds columns to a table in the database.
+     * 
+     * @param connection
+     *            The JDBC connection.
+     * @param dialect
+     *            The SQL dialect.
+     * @throws SQLException
+     *             For any SQL error.
+     * @throws AddendumException
+     *             For any error occurring during the update.
+     */
     public void execute(Connection connection, Dialect dialect) throws SQLException
     {
         for (Column column : addColumns)
