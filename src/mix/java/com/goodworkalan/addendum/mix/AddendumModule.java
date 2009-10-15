41df1f77f2dc49a7d777262aafff909baf9c6c69
Alan Gutierrez <alan@blogometer.com>
2009-10-15 01:55:02 -0500

    Mix build.

---
+++ src/mix/java/com/goodworkalan/addendum/mix/AddendumModule.java
@@ -0,0 +1,17 @@
+package com.goodworkalan.go.go.mix;
+
+import com.goodworkalan.go.go.Artifact;
+import com.goodworkalan.mix.BasicJavaModule;
+
+public class AddendumModule extends BasicJavaModule {
+    public AddendumModule() {
+        super(new Artifact("com.goodworkalan", "addendum", "0.7"));
+        addDependency(new Artifact("org.slf4j", "slf4j-api", "1.4.2"));
+        addDependency(new Artifact("com.goodworkalan", "furnish", "0.1"));
+        addDependency(new Artifact("com.goodworkalan", "prattle", "0.1"));
+        addTestDependency(new Artifact("org.slf4j", "slf4j-log4j12", "1.4.2"));
+        addTestDependency(new Artifact("log4j", "log4j", "1.2.14"));
+        addTestDependency(new Artifact("org.testng", "testng", "5.10"));
+        addTestDependency(new Artifact("org.mockito", "mockito-core", "1.6"));
+    }
+}
