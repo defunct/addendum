ba4c699e337644220e8cee68c81aac3c6c9aeba9
Alan Gutierrez <alan@blogometer.com>
2010-05-05 02:14:23 -0500

    Updated Mix build for new project language.

--- src/mix/java/com/goodworkalan/addendum/mysql/mix/AddendumMysqlProject.java
+++ src/mix/java/com/goodworkalan/addendum/mysql/mix/AddendumMysqlProject.java
@@ -1,6 +1,5 @@
 package com.goodworkalan.addendum.mysql.mix;
 
-import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
@@ -10,19 +9,19 @@ public class AddendumMysqlProject extends ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.goodworkalan/addendum-mysql/0.1"))
+                .produces("com.github.bigeasy.addendum/addendum-mysql/0.1")
                 .main()
                     .depends()
-                        .artifact(new Artifact("org.slf4j/slf4j-api/1.4.2"))
-                        .artifact(new Artifact("com.goodworkalan/addendum/0.7"))
+                        .include("org.slf4j/slf4j-api/1.4.2")
+                        .include("com.github.bigeasy.addendum/addendum/0.+7")
                         .end()
                     .end()
                 .test()
                     .depends()
-                        .artifact(new Artifact("org.slf4j/slf4j-log4j12/1.4.2"))
-                        .artifact(new Artifact("log4j/log4j/1.2.14"))
-                        .artifact(new Artifact("org.testng/testng/5.10/jdk15"))
-                        .artifact(new Artifact("org.mockito/mockito-core/1.6"))
+                        .include("org.slf4j/slf4j-log4j12/1.4.2")
+                        .include("log4j/log4j/1.2.14")
+                        .include("org.testng/testng-jdk15/5.10")
+                        .include("org.mockito/mockito-core/1.6")
                         .end()
                     .end()
                 .end()
