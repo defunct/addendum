4f0ffc09a546183c4928be4e5814759a0227fc93
Alan Gutierrez <alan@blogometer.com>
2010-06-01 22:00:49 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/addendum/jpa/mix/AddendumJpaProject.java
+++ src/mix/java/com/goodworkalan/addendum/jpa/mix/AddendumJpaProject.java
@@ -4,29 +4,33 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class AddendumJpaProject extends ProjectModule {
-    @Override
+/**
+ * Build definition for Addendum JPA.
+ *
+ * @author Alan Gutierrez
+ */
+public class AddendumJpaProject implements ProjectModule {
+    /**
+     * Build the project definition for Addendum JPA.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.addendum/addendum-jpa/0.1")
-                .main()
-                    .depends()
-                        .include("org.slf4j/slf4j-api/1.4.2")
-                        .include("com.github.bigeasy.addendum/addendum/0.+7")
-                        .include("org.hibernate/hibernate-core/3.3.1.GA")
-                        .include("org.hibernate/hibernate-annotations/3.4.0.GA")
-                        .end()
-                    .end()
-                .test()
-                    .depends()
-                        .include("org.slf4j/slf4j-log4j12/1.4.2")
-                        .include("log4j/log4j/1.2.14")
-                        .include("org.testng/testng-jdk15/5.10")
-                        .include("org.mockito/mockito-core/1.6")
-                        .include("com.h2database/h2/1.1.115")
-                        .include("com.goodworkalan/addendum-h2/0.1")
-                        .end()
+                .depends()
+                    .production("org.slf4j/slf4j-api/1.4.2")
+                    .production("com.github.bigeasy.addendum/addendum/0.+7")
+                    .production("org.hibernate/hibernate-core/3.3.1.GA")
+                    .production("org.hibernate/hibernate-annotations/3.4.0.GA")
+                    .development("org.slf4j/slf4j-log4j12/1.4.2")
+                    .development("log4j/log4j/1.2.14")
+                    .development("org.testng/testng-jdk15/5.10")
+                    .development("org.mockito/mockito-core/1.6")
+                    .development("com.h2database/h2/1.1.115")
+                    .development("com.goodworkalan/addendum-h2/0.1")
                     .end()
                 .end()
             .end();
